caaee696d226827308f024703f636e0ec1358cc4
Вадим <vadim.korolenko@blinkreaction.com>
2016-02-22 14:09:51 +0200

    Added keydown listener and handler

--- public/js/script.jsx
+++ public/js/script.jsx
@@ -1,8 +1,14 @@
 var App = React.createClass({
   getInitialState: function () {
     return {
-      word: ""
+      word: "",
+      success: [],
+      fails: []
     };
+  },
+  componentDidMount: function () {
+    document.addEventListener("keydown", this.handleKeydown);
+    this.socket.on("newWord", this.setWord);
   },
   socket: io(),
   getWord: function () {
@@ -14,13 +20,53 @@ var App = React.createClass({
 
     this.setState(state);
   },
-  componentDidMount: function () {
-    this.socket.on("newWord", this.setWord);
+  handleKeydown: function (e) {
+    var charCode = e.which || e.keyCode;
+    var char = String.fromCharCode(charCode).toLowerCase();
+
+    if (charCode >= 65 && charCode <= 90 && // key actually has letter
+      this.state.success.indexOf(char) < 0 && this.state.fails.indexOf(char) < 0) { // key not used before
+      if (this.state.word.indexOf(char) >= 0) {
+        this.succeed(char);
+      }
+      else {
+        this.failed(char);
+      }
+    }
+  },
+  succeed: function (char) {
+    this.state.success.push(char);
+    this.setState({success: this.state.success});
+  },
+  failed: function (char) {
+    this.state.fails.push(char);
+    this.setState({fails: this.state.fails});
   },
   render: function () {
-    return <div>
-      {this.state.word}
-      <div onClick={this.getWord}>Reset</div>
+
+    return <div id="app">
+      <div>
+        {this.state.word}
+        <div onClick={this.getWord}>Reset</div>
+      </div>
+
+      <div className="fails">
+        <div className="title">You missed:</div>
+        <div className="chars">{
+          this.state.fails.map(function (char) {
+            return <div className="char">{char}</div>;
+          })
+        }</div>
+      </div>
+
+      <div className="success">
+        <div className="title"></div>
+        <div className="chars">{
+          this.state.success.map(function (char) {
+            return <div className="char">{char}</div>;
+          })
+        }</div>
+      </div>
     </div>;
   }
 });
